926c65f9eb20572bd8c42e26ddda8c35df690a4e
Dimeji Rufai <rfdimeji@gmail.com>
2023-12-12 22:16:31 +0000

    create db, cloudinary & redis connection functionality

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,31 @@
-import express from "express";
+require('dotenv').config();
+import express, { Request, Response, NextFunction } from "express";
 export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+// body parser
+app.use(express.json({limit: "50mb"}));
 
-import cors from "cors"
+// cookie parser
+app.use(cookieParser());
+
+// cors => cross origin resource sharing
+app.use(cors({
+    origin: process.env.ORIGIN
+}));
+
+// testing api
+app.get("/test", (req: Request, res: Response, next:NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: "API is working",
+    })
+})
+
+// Unknon route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any
+    err.statusCode = 404;
+    next(err)
+})
