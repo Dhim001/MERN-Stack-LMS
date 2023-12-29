90e1991f15ba467dc936edf02751bb8f355487f1
Dimeji Rufai <rfdimeji@gmail.com>
2023-12-29 22:17:48 +0000

    Created a generated new accesstoken, user social authentication, get user info functionality

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,13 +1,22 @@
 import express from "express";
 import {
   activateUser,
+  getUserInfo,
   loginUser,
+  logoutUser,
   registrationUser,
+  socialAuth,
+  updateAccessToken,
 } from "../controllers/user.controller";
+import { isAutheticated } from "../middleware/auth";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/logout", isAutheticated, logoutUser);
+userRouter.get("/refresh", updateAccessToken);
+userRouter.get("/me", isAutheticated, getUserInfo);
+userRouter.post("/social-auth", socialAuth);
 
 export default userRouter;
